263e1f36e8a5ba499fe798adeb97bd49516a3e34
wangjh <wangjhstc@126.com>
2015-08-10 14:39:03 +0800

    提交工程文件

---
+++ SQLiteDemo/SQLiteDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQLiteDemo
+//
+//  Created by microsparrow on 15/8/10.
+//  Copyright (c) 2015年 microsparrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SQLiteDemo/SQLiteDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SQLiteDemo
+//
+//  Created by microsparrow on 15/8/10.
+//  Copyright (c) 2015年 microsparrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
