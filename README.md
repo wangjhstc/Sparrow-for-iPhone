b70e93966df9038ba1760368559c397771823008
wangjh <wangjhstc@126.com>
2015-08-11 13:02:19 +0800

    项目的临时修改，添加库文件

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Sparrow-for-iPhone
+
+# 1-->Sqlite 数据存储
+# 2-->存储对象化
+# 3-->单元测试
+# 4-->数据库加密
+
+
+/*
+sqlite3          *db, 数据库句柄，跟文件句柄FILE很类似
+sqlite3_stmt      *stmt, 这个相当于ODBC的Command对象，用于保存编译好的SQL语句
+sqlite3_open(),   打开数据库，没有数据库时创建。
+sqlite3_exec(),   执行非查询的sql语句
+Sqlite3_step(), 在调用sqlite3_prepare后，使用这个函数在记录集中移动。
+Sqlite3_close(), 关闭数据库文件
+还有一系列的函数，用于从记录集字段中获取数据，如
+sqlite3_column_text(), 取text类型的数据。
+sqlite3_column_blob（），取blob类型的数据
+sqlite3_column_int(), 取int类型的数据
+*/
--- SQLiteDemo/SQLiteDemo/ViewController.m
+++ SQLiteDemo/SQLiteDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <sqlite3.h>
 
 @interface ViewController ()
+{
+    sqlite3 *db;
+}
 
 @end
 
@@ -16,12 +20,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void)openDB {
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documents = [paths objectAtIndex:0];
+    NSString *database_path = [documents stringByAppendingPathComponent:@"Tabs"];
+    
+    if (sqlite3_open([database_path UTF8String], &db) != SQLITE_OK) {
+        sqlite3_close(db);
+        NSLog(@"数据库打开失败");
+    }
+}
+
+-(void)execSql:(NSString *)sql {
+    char *err;
+    if (sqlite3_exec(db, [sql UTF8String], NULL, NULL, &err) != SQLITE_OK) {
+        sqlite3_close(db);
+        NSLog(@"数据库操作数据失败!");
+    }
+}
+
+- (void)searchSql:(NSString *)sql {
+    NSString *sqlQuery = @"SELECT * FROM PERSONINFO";
+    sqlite3_stmt * statement;
+    
+    if (sqlite3_prepare_v2(db, [sqlQuery UTF8String], -1, &statement, nil) == SQLITE_OK) {
+        while (sqlite3_step(statement) == SQLITE_ROW) {
+            char *name = (char*)sqlite3_column_text(statement, 1);
+            NSString *nsNameStr = [[NSString alloc]initWithUTF8String:name];
+            
+            int age = sqlite3_column_int(statement, 2);
+            
+            char *address = (char*)sqlite3_column_text(statement, 3);
+            NSString *nsAddressStr = [[NSString alloc]initWithUTF8String:address];
+            
+            NSLog(@"name:%@  age:%d  address:%@",nsNameStr,age, nsAddressStr);
+        }
+    }
+    sqlite3_close(db);
 }
 
 @end
